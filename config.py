7ae99a7771e0893b07f5f596bc8dd29200e44420
L3xz <levitskiy060108@gmail.com>
2025-10-30 23:41:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-deepseek_free_api_key = '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'
-mistral_api_key = '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'
-linkup_api_key = '7586e399-3817-466e-8f76-984503b83b2c'
+deepseek_free_api_key = 'API KEY in https://openrouter.ai/'
+mistral_api_key = 'API KEY in https://openrouter.ai/'
+linkup_api_key = 'LINK UP API KEY'
 
-base_url = 'https://openrouter.ai/api/v1'
+base_url = 'https://openrouter.ai/api/v1' # CONST
